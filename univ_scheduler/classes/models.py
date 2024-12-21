17b4c2e7e8e1e489dc6c90a32c8c69d090acc62f
ArvinNotDev <arveenjafari19@gmail.com>
2024-12-21 23:22:18 +0330

    adding classes app

---
+++ univ_scheduler/classes/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from accounts.models import User  # Import the User model
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=10, unique=True)
+    description = models.TextField()
+    credits = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+
+class Class(models.Model):
+    course = models.ForeignKey(Course, on_delete=models.CASCADE)
+    instructors = models.ManyToManyField(User, limit_choices_to={'is_teacher': True})
+    schedule = models.CharField(max_length=100)
+    room = models.ForeignKey('Rooms', on_delete=models.SET_NULL, null=True)
+
+    def __str__(self):
+        return f"{self.course.name} - {', '.join([instructor.username for instructor in self.instructors.all()])}"
+
+
+class Rooms(models.Model):
+    name = models.CharField(max_length=50)
+    capacity = models.IntegerField()
+    location = models.CharField(max_length=100)
+
+    def __str__(self):
+        return self.name
