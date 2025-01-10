a095103cea921d70f0f8acf4ea260281cd187c4d
ArvinNotDev <arveenjafari19@gmail.com>
2025-01-10 20:27:10 +0330

    classes model modifyed

--- univ_scheduler/classes/models.py
+++ univ_scheduler/classes/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from accounts.models import User
-from departments.models import Branch
+from departments.models import Department
+
 
 class Course(models.Model):
     name = models.CharField(max_length=100)
@@ -12,20 +13,11 @@ class Course(models.Model):
         return self.name
 
 
-class Rooms(models.Model):
-    name = models.CharField(max_length=50)
-    capacity = models.IntegerField()
-    location = models.ForeignKey(Branch, on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.name
-
-
 class Class(models.Model):
+    department = models.ForeignKey(Department, on_delete=models.CASCADE)
     course = models.ForeignKey(Course, on_delete=models.CASCADE)
     instructors = models.ManyToManyField(User, limit_choices_to={'is_teacher': True})
     schedule = models.CharField(max_length=100)
-    room = models.ForeignKey(Rooms, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return f"{self.course.name} - {', '.join([instructor.username for instructor in self.instructors.all()])}"
