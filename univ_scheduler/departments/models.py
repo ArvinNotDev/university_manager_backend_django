b0da0fd572a1ca27ba9172627841d2096d293912
ArvinNotDev <arveenjafari19@gmail.com>
2025-01-10 20:27:45 +0330

    departments models modifyed

--- univ_scheduler/departments/models.py
+++ univ_scheduler/departments/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from accounts.models.User import User
 
-# Create your models here.
+
+class DepartmentManager(models.Model):
+    user = models.OneToOneField(User, limit_choices_to={"is_employee": True}, on_delete=models.CASCADE)
+    department = models.OneToOneField('Department', on_delete=models.CASCADE)
+
+
+class Department(models.Model):
+    name = models.CharField(max_length=100)
+    code = models.CharField(max_length=10, unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.name
