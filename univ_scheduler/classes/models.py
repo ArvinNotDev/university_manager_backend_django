281e25ca4714a582655cf86e1b5e5f452a0e022b
ArvinNotDev <arveenjafari19@gmail.com>
2025-01-14 00:52:10 +0330

    classes models.py updated

--- univ_scheduler/classes/models.py
+++ univ_scheduler/classes/models.py
@@ -12,26 +12,10 @@ class BaseModel(models.Model):
         abstract = True
 
 
-class Course(BaseModel):
-    name = models.CharField(max_length=100, verbose_name=_("Name"))
-    code = models.CharField(max_length=10, unique=True, verbose_name=_("Code"), db_index=True)
-    description = models.TextField(verbose_name=_("Description"))
-    credits = models.IntegerField(verbose_name=_("Credits"))
-
-    class Meta:
-        managed = True
-        verbose_name = 'Course'
-        verbose_name_plural = 'Courses'
-
-    def __str__(self):
-        return self.name
-
-
 class Class(BaseModel):
+    name = models.CharField(max_length=100, verbose_name=_("Name"))
     department = models.ForeignKey(Department, on_delete=models.CASCADE, verbose_name=_("Department"), db_index=True)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name=_("Course"), db_index=True)
-    instructors = models.ManyToManyField(User, limit_choices_to={'is_teacher': True}, verbose_name=_("Instructors"))
-    schedule = models.CharField(max_length=100, verbose_name=_("Schedule"), db_index=True)
+    description = models.TextField(verbose_name=_("Description"))
 
     class Meta:
         managed = True
@@ -40,3 +24,20 @@ class Class(BaseModel):
 
     def __str__(self):
         return f"{self.course.name} - {', '.join([instructor.username for instructor in self.instructors.all()])}"
+
+
+class Course(BaseModel):
+    name = models.CharField(max_length=100, verbose_name=_("Name"))
+    code = models.CharField(max_length=10, unique=True, verbose_name=_("Code"), db_index=True)
+    _class = models.ManyToManyField(Class, on_delete=models.CASCADE, verbose_name=_("Class"), db_index=True)
+    instructors = models.ManyToManyField(User, limit_choices_to={'is_teacher': True}, verbose_name=_("Instructors"))
+    description = models.TextField(verbose_name=_("Description"))
+    schedule = models.TimeField(verbose_name=_("Schedule"), db_index=True)
+
+    class Meta:
+        managed = True
+        verbose_name = 'Course'
+        verbose_name_plural = 'Courses'
+
+    def __str__(self):
+        return self.name
