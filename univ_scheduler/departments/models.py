fe2be9703b30bf72d7455037de38175cabd2bd07
ArvinNotDev <arveenjafari19@gmail.com>
2025-01-14 00:55:37 +0330

    departments models updated

--- univ_scheduler/departments/models.py
+++ univ_scheduler/departments/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from accounts.models.User import User
+from accounts.models import User
 from django.utils.translation import ugettext_lazy as _
 import uuid
 
@@ -12,15 +12,15 @@ class BaseModel(models.Model):
         abstract = True
 
 
-class DepartmentManager(BaseModel):
-    user = models.OneToOneField(User, limit_choices_to={"is_employee": True}, on_delete=models.CASCADE, verbose_name=_("User"), db_index=True)
-
-
 class Department(BaseModel):
     id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False, verbose_name=_("ID"), db_index=True)
     name = models.CharField(max_length=100, verbose_name=_("Name"), db_index=True)
-    department_manager = models.OneToOneField(DepartmentManager, on_delete=models.CASCADE, verbose_name=_("Department manager"), db_index=True)
     description = models.TextField(max_length=200, verbose_name=_("Description"))
 
     def __str__(self):
         return self.name
+
+
+class DepartmentManager(BaseModel):
+    user = models.OneToOneField(User, limit_choices_to={"is_employee": True}, on_delete=models.CASCADE, verbose_name=_("User"), db_index=True)
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, verbose_name=_("Department"), db_index=True)
