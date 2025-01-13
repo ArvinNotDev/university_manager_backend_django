8fb44f0ccdbe601d97b684af1ba0ab21ff7b0090
ArvinNotDev <arveenjafari19@gmail.com>
2025-01-14 00:22:44 +0330

    classes models updated

--- univ_scheduler/classes/models.py
+++ univ_scheduler/classes/models.py
@@ -1,23 +1,42 @@
 from django.db import models
 from accounts.models import User
 from departments.models import Department
+from django.utils.translation import ugettext_lazy as _
 
 
-class Course(models.Model):
-    name = models.CharField(max_length=100)
-    code = models.CharField(max_length=10, unique=True)
-    description = models.TextField()
-    credits = models.IntegerField()
+class BaseModel(models.Model):
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name=_("Created At"))
+    updated_at = models.DateTimeField(auto_now=True, verbose_name=_("Updated At"))
+
+    class Meta:
+        abstract = True
+
+
+class Course(BaseModel):
+    name = models.CharField(max_length=100, verbose_name=_("Name"))
+    code = models.CharField(max_length=10, unique=True, verbose_name=_("Code"), db_index=True)
+    description = models.TextField(verbose_name=_("Description"))
+    credits = models.IntegerField(verbose_name=_("Credits"))
+
+    class Meta:
+        managed = True
+        verbose_name = 'Course'
+        verbose_name_plural = 'Courses'
 
     def __str__(self):
         return self.name
 
 
-class Class(models.Model):
-    department = models.ForeignKey(Department, on_delete=models.CASCADE)
-    course = models.ForeignKey(Course, on_delete=models.CASCADE)
-    instructors = models.ManyToManyField(User, limit_choices_to={'is_teacher': True})
-    schedule = models.CharField(max_length=100)
+class Class(BaseModel):
+    department = models.ForeignKey(Department, on_delete=models.CASCADE, verbose_name=_("Department"), db_index=True)
+    course = models.ForeignKey(Course, on_delete=models.CASCADE, verbose_name=_("Course"), db_index=True)
+    instructors = models.ManyToManyField(User, limit_choices_to={'is_teacher': True}, verbose_name=_("Instructors"))
+    schedule = models.CharField(max_length=100, verbose_name=_("Schedule"), db_index=True)
+
+    class Meta:
+        managed = True
+        verbose_name = 'Class'
+        verbose_name_plural = 'Classes'
 
     def __str__(self):
         return f"{self.course.name} - {', '.join([instructor.username for instructor in self.instructors.all()])}"
