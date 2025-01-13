95af028539bbeb46ad40500ab6ed73ba48438fcf
ArvinNotDev <arveenjafari19@gmail.com>
2025-01-14 00:35:07 +0330

    admin panel has been updated for departments

--- univ_scheduler/departments/admin.py
+++ univ_scheduler/departments/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from models import Department, DepartmentManager
 
-# Register your models here.
+@admin.register(Department)
+class DepartmentAdmin(admin.ModelAdmin):
+    list_display = ('id', 'name', 'description', 'created_at', 'updated_at')
+    search_fields = ('name', 'description', 'created_at')
+    ordering = ('name',)
+
+@admin.register(DepartmentManager)
+class DepartmentManagerAdmin(admin.ModelAdmin):
+    list_display = ('department', 'manager_name', 'start_date')
+    search_fields = ('manager_name', 'department__name')
+    ordering = ('manager_name',)
+
