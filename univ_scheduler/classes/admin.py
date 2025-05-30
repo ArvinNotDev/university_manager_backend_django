682ef12d72b51938821c271c999994963a738546
ArvinNotDev <arveenjafari19@gmail.com>
2025-05-30 15:08:58 +0330

    fixing classes admin page and other stuff

--- univ_scheduler/classes/admin.py
+++ univ_scheduler/classes/admin.py
@@ -12,7 +12,18 @@ class ClassAdmin(admin.ModelAdmin):
 
 @admin.register(Course)
 class CourseAdmin(admin.ModelAdmin):
-    list_display = ('id', 'name', '_class', 'instructors', 'description', 'schedule', 'created_at', 'updated_at')
+    list_display = ('id', 'name', 'get_class', 'display_instructors', 'description', 'schedule', 'created_at', 'updated_at')
     list_filter = ('_class', 'instructors', 'schedule', 'created_at', 'updated_at')
     ordering = ('name', 'created_at', 'updated_at')
-    search_fields = ('name', '_class', 'instructors', 'schedule', 'created_at', 'updated_at')
+    search_fields = ('name', '_class__name', 'instructors__username', 'schedule', 'created_at', 'updated_at')
+    
+    def get_class(self, obj):
+        """Custom method to display the related class"""
+        return obj._class.name if obj._class else None
+    get_class.short_description = 'Class'
+    get_class.admin_order_field = '_class__name'
+    
+    def display_instructors(self, obj):
+        """Custom method to display instructors"""
+        return ", ".join([instructor.username for instructor in obj.instructors.all()[:3]])
+    display_instructors.short_description = 'Instructors'
--- univ_scheduler/classes/models.py
+++ univ_scheduler/classes/models.py
@@ -1,7 +1,7 @@
 from django.db import models
 from accounts.models import User
 from departments.models import Department
-from django.utils.translation import ugettext_lazy as _
+from django.utils.translation import gettext_lazy as _
 import uuid
 
 
