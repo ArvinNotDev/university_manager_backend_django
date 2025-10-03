4419856a9e7fe4a8dd3bb0925b2aeab45a073a90
ArvinNotDev <arveenjafari19@gmail.com>
2025-10-03 19:13:00 +0330

    adding admin page for attendance app

--- apps/attendances/admin.py
+++ apps/attendances/admin.py
@@ -1,3 +1,22 @@
 from django.contrib import admin
+from .models import AttendanceSession, AttendanceRecord
 
-# Register your models here.
+
+@admin.register(AttendanceRecord)
+class AttendanceRecordAdmin(admin.ModelAdmin):
+    list_display = ('student', 'session', 'present')
+    list_filter = ('present', 'session__course', 'session__class_obj')
+    search_fields = ('student__username', 'student__email', 'session__course__name', 'session__class_obj__name')
+    ordering = ('session__created_at', 'student')
+    autocomplete_fields = ('student', 'session')
+
+
+@admin.register(AttendanceSession)
+class AttendanceSessionAdmin(admin.ModelAdmin):
+    list_display = ('course', 'class_obj', 'schedule', 'created_at')
+    list_filter = ('course', 'class_obj', 'schedule')
+    search_fields = ('course__name', 'class_obj__name', 'schedule__course__name')
+    ordering = ('created_at',)
+    autocomplete_fields = ('course', 'class_obj', 'schedule')
+    inlines = []
+
